71d9d4e01eccf6c180162f6c705b43f494eaa172
Ibrahim <ibrahimelothmeni98@gmail.com>
2024-02-04 06:50:45 +0100

    finishing

--- delete.php
+++ delete.php
@@ -27,3 +27,20 @@ if (isset($_GET['id'])) {
 } else {
     exit('No ID specified!');
 }
+
+?>
+
+<?= template_header('Delete') ?>
+<div class="content delete">
+    <h2>Delete Contact #<?= $contact['id'] ?></h2>
+    <?php if ($msg) : ?>
+        <p><?= $msg ?></p>
+    <?php else : ?>
+        <p>Are you sure you want to delete contact #<?= $contact['id'] ?>?</p>
+        <div class="yesno">
+            <a href="delete.php?id=<?= $contact['id'] ?>&confirm=yes">Yes</a>
+            <a href="delete.php?id=<?= $contact['id'] ?>&confirm=no">No</a>
+        </div>
+    <?php endif; ?>
+</div>
+<?= template_footer() ?>
