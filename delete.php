184c93fd73e79e10c6ce4700fedb7fdad82869bc
Ibrahim <ibrahimelothmeni98@gmail.com>
2024-02-04 06:48:50 +0100

    the delete config

--- delete.php
+++ delete.php
@@ -1,11 +1,29 @@
 <?php
-
-
-if ( isset($_POST['delete']) && isset($_POST['user_id']) ) {
-    echo "-----------delete--------";
-    $sql = "DELETE FROM users WHERE user_id = :zip";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute(array(':zip' => $_POST['user_id']));
+include 'functions.php';
+$pdo = pdo_connect_mysql();
+$msg = '';
+// Check that the contact ID exists
+if (isset($_GET['id'])) {
+    // Select the record that is going to be deleted
+    $stmt = $pdo->prepare('SELECT * FROM contacts WHERE id = ?');
+    $stmt->execute([$_GET['id']]);
+    $contact = $stmt->fetch(PDO::FETCH_ASSOC);
+    if (!$contact) {
+        exit('Contact doesn\'t exist with that ID!');
+    }
+    // Make sure the user confirms beore deletion
+    if (isset($_GET['confirm'])) {
+        if ($_GET['confirm'] == 'yes') {
+            // User clicked the "Yes" button, delete record
+            $stmt = $pdo->prepare('DELETE FROM contacts WHERE id = ?');
+            $stmt->execute([$_GET['id']]);
+            $msg = 'You have deleted the contact!';
+        } else {
+            // User clicked the "No" button, redirect them back to the read page
+            header('Location: read.php');
+            exit;
+        }
+    }
+} else {
+    exit('No ID specified!');
 }
-
-?>
