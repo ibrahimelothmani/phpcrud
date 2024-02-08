d3fc8f3a41962a2bfc690e4fbaf52f77dbab791f
Ibrahim <ibrahimelothmeni98@gmail.com>
2024-02-08 08:35:32 +0100

    update the authenticator code

--- authenticator.php
+++ authenticator.php
@@ -5,154 +5,147 @@ function pdo_connect_mysql() {
     $DATABASE_PASS = '';
     $DATABASE_NAME = 'phpcrud';
     try {
-    return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME .
-    ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
+        return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
     } catch (PDOException $exception) {
-    // If there is an error with the connection, stop the script and display the error.
-    exit('Failed to connect to database!');
+        // If there is an error with the connection, stop the script and display the error.
+        exit('Failed to connect to database!');
     }
-    }
-
-// Connect to MySQL database
+}
 $pdo = pdo_connect_mysql();
-session_start();
 
-if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
-    echo "Check if the user is already logged in";
-    header("Location : index.php");
+
+// Check if the user is already logged in
+if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true){
+    header("Location: index.php");
     exit;
 }
 
-// check if the form is submitted 
-if (isset($_POST['username']) && isset($_POST['password'])) {
-    // echo "Check if the form is submitted";
+
+// Check if the form is submitted
+if(isset($_POST['username']) && isset($_POST['password'])){
+    // Validate credentials
     $username = $_POST['username'];
     $password = $_POST['password'];
-}
-// Validate credentials
-$stmt = $pdo->prepare('SELECT email, username, password FROM contacts WHERE username = :username');
-$stmt->execute(['username' => $username]);
-$user = $stmt->fetch();
 
-if ($user) {
-    if ($password = $user['password']) {
-        session_start();
-        $_SESSION['loggedin'] = true;
-        $_SESSION['email'] = $user['email'];
-        $_SESSION['username'] = $user['username'];
-        header('Location: index.php');
-        exit;
+
+     // Prepare and execute the query
+     $stmt = $pdo->prepare("SELECT email, username, password FROM contacts WHERE username = :username");
+     $stmt->execute(['username' => $username]);
+ 
+     // Fetch user data
+     $user = $stmt->fetch();
+
+
+
+
+     if($user){
+        // Verify password
+        if($password == $user['password']){
+            // Password is correct, start a new session
+            session_start();
+            // Store data in session variables
+            $_SESSION['loggedin'] = true;
+            $_SESSION['email'] = $user['email'];
+            $_SESSION['username'] = $user['username'];
+            // Redirect user to dashboard page
+            header("Location: index.php");
+            exit;
+        } else {
+            // Password is incorrect
+            $error = "Invalid password";
+        }
+    } else {
+        // User not found
+        $error = "User not found";
     }
-} else {
-    // Password is incorrect
-    $error = "Invalid password";
 }
-
 ?>
 
 
 <!DOCTYPE html>
 <html>
-
 <head>
-    <meta charset="utf-8">
-    <title>$title</title>
-    <link href="style.css" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
-</head>
-
-<body>
-    <nav class="navtop">
-        <div>
-            <h1>Website Title</h1>
-            <a href="index.php"><i class="fas fa-home"></i>Home</a>
-            <a href="read.php"><i class="fas fa-address-book"></i>Contacts</a>
-            <a href="authenticator.php"><i class="fas fa-address-book"></i>Login</a>
-        </div>
-    </nav>
-</body>
-
-</html>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
-
 <body>
-    <!-- Pills navs -->
-    <ul class="nav nav-pills nav-justified mb-3 mt-5 ms-5 me-5" id="ex1" role="tablist">
-        <li class="nav-item" role="presentation">
-            <a class="nav-link active" id="tab-login" data-mdb-toggle="pill" href="#pills-login" role="tab" aria-controls="pills-login" aria-selected="true">Login</a>
-        </li>
-        <li class="nav-item" role="presentation">
-            <a class="nav-link" id="tab-register" data-mdb-toggle="pill" href="#pills-register" role="tab" aria-controls="pills-register" aria-selected="false">Register</a>
-        </li>
-    </ul>
-    <!-- Pills navs -->
 
-    <!-- Pills content -->
-    <div class="tab-content">
-        <div class="tab-pane fade show active" id="pills-login" role="tabpanel" aria-labelledby="tab-login">
-            <form method="post">
-                <div class="text-center mb-3">
-                    <p>Sign in with:</p>
+
+<div class="container">
+    <div class="row justify-content-center align-items-center g-2">
+        <div class="col-6">
+            <h1 class="display-1">Login</h1>
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                <!-- username input -->
+                <div class="form-outline mb-4">
+                    <input type="text" name="username" id="form2Example1" class="form-control" />
+                    <label class="form-label" for="form2Example1">username</label>
+                </div>
+
+
+                <!-- Password input -->
+                <div class="form-outline mb-4">
+                    <input type="password" name="password" id="form2Example2" class="form-control" />
+                    <label class="form-label" for="form2Example2">Password</label>
+                </div>
+
+
+                <!-- 2 column grid layout for inline styling -->
+                <div class="row mb-4">
+                    <div class="col d-flex justify-content-center">
+                    <!-- Checkbox -->
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="form2Example31" checked />
+                        <label class="form-check-label" for="form2Example31"> Remember me </label>
+                    </div>
+                    </div>
+
+
+                    <div class="col">
+                    <!-- Simple link -->
+                    <a href="#!">Forgot password?</a>
+                    </div>
+                </div>
+
+
+                <!-- Submit button -->
+                <button type="submit" class="btn btn-primary btn-block mb-4" name="login">Sign in</button>
+
+
+                <!-- Register buttons -->
+                <div class="text-center">
+                    <p>Not a member? <a href="#!">Register</a></p>
+                    <p>or sign up with:</p>
                     <button type="button" class="btn btn-link btn-floating mx-1">
                         <i class="fab fa-facebook-f"></i>
                     </button>
+
 
                     <button type="button" class="btn btn-link btn-floating mx-1">
                         <i class="fab fa-google"></i>
                     </button>
 
+
                     <button type="button" class="btn btn-link btn-floating mx-1">
                         <i class="fab fa-twitter"></i>
                     </button>
+
 
                     <button type="button" class="btn btn-link btn-floating mx-1">
                         <i class="fab fa-github"></i>
                     </button>
                 </div>
+                </form>
+            </div>
+            <?php if(isset($error)) { ?>
+                    <div><?php echo $error; ?></div>
+            <?php } ?>
 
-                <p class="text-center">or:</p>
 
-                <!-- Username input -->
-                <div class="form-outline mb-4">
-                    <input type="text" id="username" class="form-control ms-5 me-5" name="username" />
-                    <label class="form-label ms-5 me-5" for="username">Email or Username</label>
-                </div>
-
-                <!-- Password input -->
-                <div class="form-outline mb-4">
-                    <input type="password" id="password" class="form-control ms-5 me-5" name="password" />
-                    <label class="form-label ms-5 me-5" for="password">Password</label>
-                </div>
-
-                <!-- Submit button -->
-                <button type="button" class="btn btn-primary btn-block mb-4 ms-5 me-5">Sign in</button>
-
-                <!-- Register buttons -->
-                <div class="text-center">
-                    <p>Not a member? <a href="#!">Register</a></p>
-                </div>
-            </form>
-        </div>
-        <div class="tab-pane fade" id="pills-register" role="tabpanel" aria-labelledby="tab-register">
-            <!-- Registration form content -->
-            <!-- You can implement the registration form similar to the login form -->
-        </div>
     </div>
+   
+</div>
 
-    <?php if (isset($error)) { ?>
-        <div><?php echo $error; ?></div>
-    <?php } ?>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
-
 </html>
