c1e432f414ed8ae7a849e54333d25631c665bd57
Ibrahim <ibrahimelothmeni98@gmail.com>
2024-02-08 08:32:36 +0100

    modify the code in the functions file

--- functions.php
+++ functions.php
@@ -1,45 +1,50 @@
 <?php
-function pdo_connect_mysql() {
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'phpcrud';
-try {
-return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME .
-';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
-} catch (PDOException $exception) {
-// If there is an error with the connection, stop the script and display the error.
-exit('Failed to connect to database!');
+session_start();
+if(!isset($_SESSION['loggedin'])){
+    // Redirect to the auth page
+    header("Location: auth.php");
+    exit;
 }
+function pdo_connect_mysql() {
+    $DATABASE_HOST = 'localhost';
+    $DATABASE_USER = 'root';
+    $DATABASE_PASS = '';
+    $DATABASE_NAME = 'phpcrud';
+    try {
+        return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
+    } catch (PDOException $exception) {
+        // If there is an error with the connection, stop the script and display the error.
+        exit('Failed to connect to database!');
+    }
 }
 function template_header($title) {
-// EOT : 
-https://www.php.net/manual/en/language.types.string.php#language.types.string.syntax.heredoc
+    // EOT : https://www.php.net/manual/en/language.types.string.php#language.types.string.syntax.heredoc
 echo <<<EOT
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="utf-8">
-<title>$title</title>
-<link href="style.css" rel="stylesheet" type="text/css">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
-</head>
-<body>
-<nav class="navtop">
-<div>
-<h1>Website Title</h1>
-<a href="index.php"><i class="fas fa-home"></i>Home</a>
-<a href="read.php"><i class="fas fa-address-book"></i>Contacts</a>
-<a href="logout.php">
-<button type="button" class="btn btn-danger">Logout</button>
-</a>
-</div>
-</nav>
+    <head>
+        <meta charset="utf-8">
+        <title>$title</title>
+        <link href="style.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">  
+    </head>
+    <body>
+    <nav class="navtop">
+        <div>
+            <h1>Website Title</h1>
+            <a href="index.php"><i class="fas fa-home"></i>Home</a>
+            <a href="read.php"><i class="fas fa-address-book"></i>Contacts</a>
+            <a href="logout.php">
+                <button type="button" class="btn btn-danger">Logout</button>
+            </a>
+        </div>
+    </nav>
 EOT;
 }
 function template_footer() {
 echo <<<EOT
-</body>
+    </body>
 </html>
 EOT;
 }
